ad9b0fbcd86f4ad421e0f0d747a38b440281e0c2
elfx505 <stetco.felix@gmail.com>
2026-08-03 19:53:32 +0300

    add zoom in objective colliders for pipes inside printer drawers

--- GDT_Q3-4/Assets/Scripts/InteractableObjects/DefectivePrinterDrawer.cs
+++ GDT_Q3-4/Assets/Scripts/InteractableObjects/DefectivePrinterDrawer.cs
@@ -1,10 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DefectivePrinterDrawer : InteractableObject
 {
     [SerializeField] private int drawerIndex;
 
-    private bool isOpen = false;
+    [Header("Inner Drawer Items")]
+    // Drag the colliders of any items inside this drawer into this array in the Inspector
+    private Collider[] innerItemColliders; 
+
+    private void Start()
+    {
+        innerItemColliders = GetOnlyChildColliders();
+        
+        SetInnerItemsInteractable(false);
+    }
+
+    public void SetInnerItemsInteractable(bool isInteractable)
+    {
+        foreach (Collider item in innerItemColliders)
+        {
+            if (item != null)
+            {
+                item.enabled = isInteractable;
+            }
+        }
+    }
 
     protected override void PerformAction()
     {
@@ -16,5 +37,24 @@ public class DefectivePrinterDrawer : InteractableObject
         }
 
         PrinterPuzzleManager.Instance.OpenDrawer(drawerIndex);
+    }
+
+    // Helper function since GetComponentsInChildren also take the parent's selected component 
+    private Collider[] GetOnlyChildColliders()
+    {
+        Collider[] allColliders = GetComponentsInChildren<Collider>();
+        
+        List<Collider> childCollidersOnly = new List<Collider>();
+
+        foreach (Collider col in allColliders)
+        {
+            // If the collider's GameObject is NOT this GameObject, it's a child
+            if (col.gameObject != this.gameObject)
+            {
+                childCollidersOnly.Add(col);
+            }
+        }
+
+        return childCollidersOnly.ToArray();
     }
 }
--- GDT_Q3-4/Assets/Scripts/Pipe/PrinterPuzzleManager.cs
+++ GDT_Q3-4/Assets/Scripts/Pipe/PrinterPuzzleManager.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 
+using System.Collections;
+
 public class PrinterPuzzleManager : Singleton<PrinterPuzzleManager>
 {
     
     public int currentOpenDrawerIndex;
 
     private Animator animator;
+
+    [Header("Drawer References")]
+    public DefectivePrinterDrawer[] allDrawers; 
+
+    [Header("Settings")]
+    [Tooltip("How long the opening animation takes in seconds")]
+    public float openAnimationDuration = 1.0f; // Adjust this in the Inspector to match your animation length
 
     void Start()
     {
@@ -20,7 +29,12 @@ public class PrinterPuzzleManager : Singleton<PrinterPuzzleManager>
 
 
     public void OpenDrawer(int clickedDrawerIndex)
-    {
+    {   
+        if (currentOpenDrawerIndex != 0)
+        {
+            allDrawers[currentOpenDrawerIndex - 1].SetInnerItemsInteractable(false);
+        }
+
         // If the drawer we clicked is ALREADY the open one, close it
         if (currentOpenDrawerIndex == clickedDrawerIndex)
         {
@@ -33,7 +47,28 @@ public class PrinterPuzzleManager : Singleton<PrinterPuzzleManager>
         }
 
         animator.SetInteger("ActiveDrawer", currentOpenDrawerIndex);
+
+        if (currentOpenDrawerIndex != 0)
+        {
+            StartCoroutine(EnableItemsAfterDelay(currentOpenDrawerIndex, openAnimationDuration));
+        }
     
     }
+
+    private IEnumerator EnableItemsAfterDelay(int targetDrawerIndex, float delay)
+    {
+        // Wait for the animation duration
+        yield return new WaitForSeconds(delay);
+        
+        // SAFETY CHECK: Ensure the player didn't click a different drawer while we were waiting.
+        // We only enable the colliders if this drawer is STILL the active one.
+        if (currentOpenDrawerIndex == targetDrawerIndex)
+        {
+            // Array index is the drawer number minus 1
+            allDrawers[targetDrawerIndex - 1].SetInnerItemsInteractable(true);
+        }
+    }
+
+    
 
 }
