8b1971f1e7444e69762336c73a3321f4c062ce3d
elfx505 <stetco.felix@gmail.com>
2026-08-05 20:32:17 +0300

    randomize Janitor Lock Combination and Update the text in the boss's office to reflect the change on start

--- GDT_Q3-4/Assets/Scripts/InteractableObjects/NumpadLock.cs
+++ GDT_Q3-4/Assets/Scripts/InteractableObjects/NumpadLock.cs
@@ -1,28 +1,51 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class NumpadLock : MonoBehaviour
 {
 
 
-    private int[] correctCombination;
+    [SerializeField] private int[] correctCombination = new int[4];
     private List<int> currentCombination;
     [SerializeField] private int maxCombinationSize = 4;
     [SerializeField] TextMeshPro text;
+    [SerializeField] TextMeshPro bossMonitorText;
 
     
     void Awake()
     {
+        for (int i = 0; i < correctCombination.Length; i++)
+        {
+            correctCombination[i] = UnityEngine.Random.Range(0, 10);
+        }
         
-        correctCombination = new int[] {6, 0, 3, 2};
-
         currentCombination = new List<int>();
 
         if (text == null) Debug.LogWarning("[NumpadLock] Text object not assigned!");
     }
+
+    void Start()
+    {
+        UpdateMonitorTextWithCorrectCombination(correctCombination);
+    }
+
+    private void UpdateMonitorTextWithCorrectCombination(int[] combination)
+    {
+        string newText = "";
+
+        foreach (int i in combination)
+        {
+            newText += i.ToString();
+        }
+
+        Debug.Log(newText);
+
+        bossMonitorText.text = newText;
+    }
+
 
     public void RegisterInput(int value)
     {
