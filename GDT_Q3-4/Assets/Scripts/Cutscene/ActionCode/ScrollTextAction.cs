0b6e36869249ad45a825c6390412efbf2376bf88
HabeebMOH <h.mohammed@student.tue.nl>
2026-08-29 18:39:19 +0200

    game ending credits scroll

---
+++ GDT_Q3-4/Assets/Scripts/Cutscene/ActionCode/ScrollTextAction.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class ScrollTextAction : CutsceneAction
+{
+    [SerializeField] private TextMeshProUGUI textToScroll;
+    [SerializeField] private float scrollToPositions;
+    [SerializeField] private float speed;
+    public override IEnumerator Play(CutsceneContext context)
+    {
+        float currentPosition = textToScroll.rectTransform.anchoredPosition.y;
+        float moveDirection = (scrollToPositions - currentPosition) > 0 ? 1f : -1f;
+
+        while (Math.Abs(currentPosition - scrollToPositions) > 0.5f)
+        {
+            Debug.Log(textToScroll.rectTransform.anchoredPosition.y);
+            currentPosition += speed * moveDirection * Time.deltaTime;
+
+            Vector2 position = textToScroll.rectTransform.anchoredPosition;
+            position.y = currentPosition;
+            textToScroll.rectTransform.anchoredPosition = position;
+            yield return null;
+        }
+    }
+}
